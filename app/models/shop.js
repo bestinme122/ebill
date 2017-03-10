a31ab0721d62adcc17784f364dd62cdc60f475c1
Nguyen Viet Trong <trong.nguyen@jmango360.com>
2017-03-11 04:44:47 +0700

    Move front end from backend

--- app/models/shop.js
+++ app/models/shop.js
@@ -2,6 +2,7 @@ var Sequelize = require('sequelize'),
   Const = require('../base/enum');
 module.exports = function (sequelize) {
   var Shop = sequelize.define('Shop', {
+    // id:{type: Sequelize.INTEGER, allowNull: false, unique: true},
     name: {type: Sequelize.STRING(250), allowNull: false},
     ownerId: {type: Sequelize.STRING, allowNull: false},
     address: Sequelize.STRING,
